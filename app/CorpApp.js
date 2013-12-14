9bb794de9b7e9511222af85c3838d63d44c35575
cliuxinxin <cliuxinxin@gmail.com>
2013-12-14 17:36:47 +0800

    公司管理模块
    1、完成树的显示
    2、实现按钮

--- app/CorpApp.js
+++ app/CorpApp.js
@@ -1,5 +1,5 @@
-define(["dojo/_base/declare","dijit/layout/BorderContener","dijit/layout/ContentPane","app/MenuTree"], 
-    function(declare,BorderContener,ContentPane,MenuTree){
+define(["dojo/_base/declare","dijit/layout/BorderContainer","dijit/layout/ContentPane","app/MenuTree","dijit/form/Button"], 
+    function(declare,BorderContainer,ContentPane,MenuTree,Button){
   return declare(null, {
     constructor: function(){
     },
@@ -8,7 +8,10 @@ define(["dojo/_base/declare","dijit/layout/BorderContener","dijit/layout/Content
     startup:function(){
     	console.log("公司管理程序正在启动中" );
 
-        var corpApp = new BorderContener({});
+        var corpApp = new BorderContainer({
+            title:"公司管理",
+            selected:true
+        });
 
 
         //没有数据，先用功能树代替
@@ -20,11 +23,21 @@ define(["dojo/_base/declare","dijit/layout/BorderContener","dijit/layout/Content
             region:"left",
             content:myCorpTree
         });
+        corpApp.addChild(corpAppLeft);
 
         var corpAppCenter = new ContentPane({
             region:"center",
-            content:"这里以后是修改表单"
+            content:"这里以后是修改表单<br/>"
         });
+        corpApp.addChild(corpAppCenter);
+
+        var testButton = new Button({
+            label:"点击BIRD",
+            onClick:function(){
+                console.log("木有事可8要乱点bird")
+            }
+        });
+        corpAppCenter.addChild(testButton);
 
         return corpApp;
 
